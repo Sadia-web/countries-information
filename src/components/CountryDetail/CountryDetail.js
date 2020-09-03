1934cfcee2c4473cc1a46d94dd59eb75b7918e11
Sadia-web <sadiamim354@gmail.com>
2020-09-04 00:12:14 +0600

    done

---
+++ src/components/CountryDetail/CountryDetail.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+const CountryDetail = () => {
+    const {countryName} = useParams();
+
+    const [ countryDetails, setCountryDetails ] = useState({})
+
+    useEffect( () => {
+        fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
+        .then(res => res.json())
+        .then(data => setCountryDetails(data[0]))
+    }, [])
+    
+    console.log(countryDetails);
+    const {name, capital, population} = countryDetails;
+    
+    
+    return (
+        <div>
+            <h3>Details of {name}</h3>
+            <p>Capital: {capital}</p>
+            <p>Population: {population}</p>
+        </div>
+    );
+};
+
+export default CountryDetail;
